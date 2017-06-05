db7807179ccc4cb54b89278ad1e634042e3b7fdc
Julie Allinson <julie.allinson@london.ac.uk>
2017-06-05 21:59:08 +0100

    hyku aws script improvements

--- aws_hyku/provision_hyku_aws.sh
+++ aws_hyku/provision_hyku_aws.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 RAILS_MODE="production"
-SOLR="35.157.250.34"
+USER="centos"
+BRANCH="master"
+REPO="https://github.com/projecthydra-labs/hyku"
 
+# Add the user, create database and grant all privileges
+sudo -u postgres bash -c "psql -c \"CREATE USER $USER WITH PASSWORD '$USER';\""
+sudo -u postgres bash -c "psql -c \"ALTER USER $USER CREATEDB;\""
+
+# Clone hyku
+cd /opt
+if [ ! -d hyku ]
+then
+  echo 'Cloning hyku'
+  sudo git clone $REPO
+else
+  echo 'hyku is already cloned, moving on ... '
+fi
+
+sudo chown -R $USER:$USER /opt/hyku
 cd /opt/hyku
+git checkout $BRANCH
+
+echo 'Running bundler'
+# error with rainbow needs gem update --system
+gem update --system
+bundle install
 
 # TODO make it so these aren't necessary
 #mv /home/centos/install_files/fedora.yml config/fedora.yml
@@ -18,13 +41,13 @@ mv /home/centos/install_files/rbenv-vars .rbenv-vars
 rm -r /home/centos/install_files
 
 echo 'turn off multitenancy'
-sudo sed -i 's/true/false/' /config/settings/production.yml
+sudo sed -i 's/true/false/' config/settings/production.yml
 echo 'insert solr url into settings'
-sudo sed -i 's/127.0.0.1/$SOLR/' /config/settings.yml
+sudo sed -i 's/127.0.0.1/35.157.250.34/' config/settings.yml
+sudo sed -i 's/host:/#host:/' config/database.yml
+sudo sed -i 's/port:/#port:/' config/database.yml
 
-echo 'Running bundler and db:migrate'
-# old error with rainbow needed this; no harm in doing it now
-gem update --system
+echo 'Running setup'
 RAILS_ENV=$RAILS_MODE bin/setup
 # bundle install# bundle install
 #rake db:create RAILS_ENV=$RAILS_MODE
@@ -33,12 +56,7 @@ RAILS_ENV=$RAILS_MODE bin/setup
 #rake hyrax:workflow:load RAILS_ENV=$RAILS_MODE
 
 # TODO only do this for production
-if [ "$RAILS_MODE" = "production" ]
-then
-    RAILS_ENV=$RAILS_MODE rake assets:precompile
-else
-    echo '$RAILS_MODE mode, no need to precompile'
-fi
+RAILS_ENV=$RAILS_MODE rake assets:precompile
 
 DISABLE_REDIS_CLUSTER=true bundle exec sidekiq -d -L log/sidekiq.log -e $RAILS_MODE
 DISABLE_REDIS_CLUSTER=true bundle exec puma -e $RAILS_MODE -p 3000 --pidfile tmp/pids/puma.pid -d
